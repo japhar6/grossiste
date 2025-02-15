25d9055a024a72036cbda2daccb9b8cbde9271c4
eddyrakotonirina06 <eddyrakotonirina06@gmail.com>
2025-02-15 09:58:45 +0300

    prémier connexion au base de donné

--- back_grossiste/server.js
+++ back_grossiste/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const bcrypt = require('bcryptjs');
 require("dotenv").config();
 
 
