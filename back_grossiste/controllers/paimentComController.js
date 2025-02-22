6f74e40ed96c44776dfd55f1fb2e3dc0dbf544be
NanasDev10 <nanassykes@gmail.com>
2025-02-22 15:43:50 +0300

    maj

--- back_grossiste/controllers/paimentComController.js
+++ back_grossiste/controllers/paimentComController.js
@@ -143,14 +143,36 @@ exports.getVentesByCommercial = async (req, res) => {
     }
 };
 
+exports.getVentesByInfo = async (req, res) => {
+    try {
+        const { commercialId, commandeId } = req.params;
+        
+        // Assurez-vous de filtrer par référence de facture (ou un autre critère unique)
+        const ventes = await VenteCom.find({ commercialId, commandeId })
+            .populate("produitsRestants.produitId", "nom")
+            .populate("produitsVendus.produitId", "nom");
+
+        if (!ventes || ventes.length === 0) {
+            return res.status(404).json({ message: "Aucune vente trouvée pour ce commercial et cette commande" });
+        }
+
+        res.status(200).json(ventes);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+
 exports.getPaiementsCommerciales = async (req, res) => {
     try {
         const paiements = await PaiementCommerciale.find()
-            .populate("commandeId", "referenceFacture modePaiement statut date commercialId")
+            .populate("commandeId", "referenceFacture modePaiement statut date commercialId commandeId")
             .populate("idCaissier", "nom")
 
 
         const paiementsFormatted = paiements.map(paiement => ({
+            commandeId: paiement.commandeId || "N/A",
             referenceFacture: paiement.commandeId?.referenceFacture || "N/A",
             caissier: paiement.idCaissier ? `${paiement.idCaissier.nom}` : "Inconnu",
             modePaiement: paiement.commandeId?.modePaiement || "Non défini",
